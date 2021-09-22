7527517a3b444f8ad39c81ab8f4b0f98af776637
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-21 22:02:24 -0700

    login function

---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+// login form handler // 
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+// get query data from login form // 
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+// create user input into string // 
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector('#login-form')
+  .addEventListener('submit', loginFormHandler);
