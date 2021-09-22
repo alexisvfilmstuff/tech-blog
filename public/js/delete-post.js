a1d85f945a163091f292bc6a60df759255744317
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-21 22:08:17 -0700

    added delete post function

---
+++ public/js/delete-post.js
@@ -0,0 +1,24 @@
+async function delButtonHandler(event) {
+  event.preventDefault();
+
+  // get post and id from the url // 
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+// delete post // 
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE',
+  });
+
+// delete redirects to dashboard otherwise show error // 
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector('.delete-post-btn')
+  .addEventListener('click', delButtonHandler);
