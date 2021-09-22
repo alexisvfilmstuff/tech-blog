5e7f28e6fe28c52500c1860dbc9bb8b2bc4e7a6e
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-21 22:13:59 -0700

    fixed format

--- public/js/singup.js
+++ public/js/singup.js
@@ -2,11 +2,11 @@ async function signupFormHandler(event) {
   event.preventDefault();
 
 // get query data from sign up form // 
-  const username = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+const username = document.querySelector('#username-signup').value.trim();
+const password = document.querySelector('#password-signup').value.trim();
 
 // create input into string and prompt to dashboard // 
-  if (username && password) {
+if (username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({
