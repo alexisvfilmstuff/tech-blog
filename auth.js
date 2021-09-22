f3acaef3aad818511748139d514fb1fb1120e069
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-21 20:40:53 -0700

    created login requestion js

---
+++ auth.js
@@ -0,0 +1,11 @@
+// login request to login route // 
+
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
